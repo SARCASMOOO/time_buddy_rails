af0eb10af8a3fa7b8464e7fe2128f17d23b0b47e
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-07-04 11:23:11 -0400

    Working on the guard installation.

---
+++ spec/support/create_new_schedule.rb
@@ -0,0 +1,10 @@
+RSpec.describe "creating a new schedule", type: :system do
+  it "allows a user to create a schedule", :pending do
+    false
+    # visit new_user_registration
+    # fill_in "Name", with: "Project Runway"
+    # fill_in "Tasks", with: "Choose Fabric:3\nMake it Work:5" click_on("Create Project")
+    # visit new_user_registration
+    # expect(page).to have_content("Project Runway") expect(page).to have_content(8)
+  end
+end
---
+++ spec/support/system.rb
@@ -0,0 +1,3 @@
+RSpec.configure do |config| config.before(:each, type: :system) do
+  driven_by :rack_test end
+end
